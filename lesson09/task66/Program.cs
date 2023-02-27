b8e7226a3e8691c2da28609891aa2875625dc4f8
FisayAA <fisay@fisay.ru>
2023-02-27 22:28:02 +0300

    lesson 9 tasks 66 & 68

---
+++ lesson09/task66/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+
+int SummRange (int firstNumber, int secondNumber) // Метод расчет сумы чисел заданного диапазона
+{
+    int summ = firstNumber;    
+    if (secondNumber > firstNumber) summ += SummRange(firstNumber+1, secondNumber) ;
+    return summ;
+}
+
+Console.Clear();
+
+Console.Write("Введите начальное число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите конечное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (m > n) // Если пользователь ввел числа не по возрастанию, меняем местами
+    {
+        int temp = m;
+        m = n;
+        n = temp;
+    }
+
+Console.Write($"Сумма натуральных элементов в промежутке от {m} до {n} = {SummRange(firstNumber: m, secondNumber: n)}"); // Вывод результата расчета в консоль
---
+++ lesson09/task68/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29 */
+
+int fAccermana (int firstNumber, int secondNumber) // Метод расчета функции Аккермана
+{
+    int result = 0;
+    if(firstNumber == 0) result = secondNumber + 1;
+    else if (secondNumber == 0) result = fAccermana(firstNumber - 1, 1);
+    else result = fAccermana(firstNumber - 1, fAccermana(firstNumber, secondNumber-1));
+    return result;
+}
+
+
+Console.Clear();
+Console.Write("Введите аргументы функции Аккермана разделив значения пробелом: ");
+int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+int m = size[0];
+int n = size[1];
+
+Console.Write($"Результат вычисления функции Аккермана: {fAccermana(firstNumber: m, secondNumber: n)}");
